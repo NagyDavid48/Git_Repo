2759739e8e32851d9991f6d882033ea9e6fd53cb
CsTomi <cstom@sch.bme.hu>
2015-03-22 23:30:08 +0100

    Update Ragacs.java

--- Ragacs.java
+++ Ragacs.java
@@ -2,6 +2,9 @@ package Class_Diagram;
 
 public class Ragacs extends Akadaly {
 
+	public Ragacs(string nev){
+		this.obinev = nev;
+	}
 	/**
 	 * A robot sebesség vektora a felére csökken.
 	 * @param r
