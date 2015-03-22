3dd4c4829ec772eae8eaef0b679ff6bfca7a1176
CsTomi <cstom@sch.bme.hu>
2015-03-22 22:32:31 +0100

    Update Ragacs.java

--- Ragacs.java
+++ Ragacs.java
@@ -1,23 +1,14 @@
-package Class_Diagram;
+package akadaly;
 
 public class Ragacs extends Akadaly {
 
 	/**
-	 * 
+	 * A robot sebesség vektora a felére csökken.
 	 * @param r
 	 */
 	public void viselkedes(Robot r) {
-		// TODO - implement Ragacs.viselkedés
-		throw new UnsupportedOperationException();
+		System.out.println("Ragacs viselkedes fv. kezdodik r parameterrel.");
+		r.ragacsraLepett();
+		System.out.println("Ragacs viselkedes fv. vege.");
 	}
-
-	/**
-	 * 
-	 * @param r
-	 */
-	public void viselkedes(Robot r) {
-		// TODO - implement Ragacs.viselkedés
-		throw new UnsupportedOperationException();
-	}
-
 }
