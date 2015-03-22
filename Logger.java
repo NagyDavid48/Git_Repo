cdb4378e1cf3b945d8825e5fd8c7a34c711b03b7
jakybalazs <jakybalazs@gmail.com>
2015-03-22 23:31:00 +0100

    Create Logger.java

---
+++ Logger.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+
+public class Logger {
+	
+	//hány tabot kell kiírni
+	public static int tabok=0;
+	//a parancs beolvasásához
+	static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	//a kiinduló objektumok kiíratásához
+	static ArrayList<String> objektumok = new ArrayList<String>();
+	
+	//metódushívás logolása
+	//használat: metódusok elején meghívni a saját objektumnevével és a metódus nevével
+	public static void logMetodusStart(String objneve, String metneve){
+		tabok++;
+		System.out.println(tabKiir() + "-> " + objneve + "." + metneve);
+	}
+	
+	//visszatérés logolása
+	//használat: metódusok végén meghívni a visszatérési értékkel
+	public static void logMetodusReturn(String visszaertek){
+		System.out.println(tabKiir() + "<- " + visszaertek);
+		tabok--;
+	}
+	
+	//tabok kiírására, annyi tabbal tér vissza, ahányadik szinten vagyunk
+	public static String tabKiir(){
+		StringBuilder sb = new StringBuilder();
+		sb.append("");
+		for (int i=0; i<tabok; i++){
+			sb.append("    ");
+		}
+		return sb.toString();
+	}
+	
+	//szekvenciainicializálásnál a kezdő objektumok kiírására
+	public static void objKiir(){
+		for(int i=0;i<objektumok.size();i++){
+			System.out.println("    "+objektumok.get(i));
+		}
+	}
+	
+	//main függvény: menüt kiírja, végrehajtja a szekvenciákat
+	public static void main(String[] args) throws IOException{
+		int menupont;
+		
+		System.out.print("Adja meg a parancs kódját : ");
+		menupont = readMenu();
+		switch(menupont){
+		
+//		template
+//		case sorszám:
+//			Tipus obj = new Tipus();
+//			objektumok.add("obj : Tipus");
+//			System.out.println("Kiindulási objektumok:");
+//			objKiir();
+//			első metódus meghívása
+//			break;
+			
+		case 8:
+			Palya p = new Palya( argumentumok );
+			objektumok.add("p : Palya");
+			Mezo m = new Mezo( argumentumok );
+			objektumok.add("m : Mezo");
+			Robot r = new Robot( argumentumok );
+			objektumok.add("r : Robot");
+			System.out.println("Kiindulási objektumok:");
+			objKiir();
+			r.toString();
+			break;
+		default:
+			System.out.println("Nincs ilyen számú szekvenciadiagram");
+		}
+		
+	}
+	
+	public static int readMenu() throws IOException{
+		return Integer.parseInt(reader.readLine());
+		
+	}
+
+}
