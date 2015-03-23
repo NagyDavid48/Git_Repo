da157da29382a6932019fea56129c5bcbf7de8c9
CsTomi <cstom@sch.bme.hu>
2015-03-23 02:18:29 +0100

    Update Palya_Menedzser.java
    Fölös kommentek kiszedése, konstruktor kommentezése. Új inicializációs konstruktor írása.

--- Palya_Menedzser.java
+++ Palya_Menedzser.java
@@ -19,31 +19,37 @@ public class Palya_Menedzser {
 	public String obinev;
 	
 	/**
+	 * A konstruktorban a pálya adatait  is meg kell adni.
 	 * 
-	 * @param kor
-	 * @param ragacs
-	 * @param olaj
+	 * @param kor - A kőrök száma
+	 * @param mag - Pálya magassága
+	 * @param szel - Pálya szélessége
+	 * @param ragacs - Robotok ragacs száma
+	 * @param olaj - Robotok olaj száma
+	 * @param nev - Objektum név
 	 */
 	public Palya_Menedzser(int kor, int mag, int szel, int rob, int ragacs, int olaj, string nev) {
-		//System.out.println("");
 		this.kor = kor;
 		this.ragacskeszlet = ragacs;
 		this.olajkeszlet = olaj;
 		this.obinev = nev;
 		palya = new Palya(mag, szel, rob, olaj, ragacs);
-		//System.out.println("");
+	}
+	
+	public Palya_Menedzser(int kor, int ragacs, int olaj, string nev) {
+		this.kor = kor;
+		this.ragacskeszlet = ragacs;
+		this.olajkeszlet = olaj;
+		this.obinev = nev;
 	}
 
 	public void korSzamol() {
-		//System.out.println("PM - korSzamol fv. kezdodik.");
 		// ... Kőr számlálás
 		// ... Valami feltétel
-		System.out.println("korszamol");
-		//System.out.println("korSzamol vege.");
+		System.out.println("korszamol")
 	}
 
 	public int getKor() {
-		//System.out.println("PM - getKor fv. kezdodik és "+this.kor+" ertekkel ter vissza.");
 		return this.kor;
 	}
 
@@ -52,9 +58,7 @@ public class Palya_Menedzser {
 	 * @param kor
 	 */
 	public void setKor(int kor) {
-		//System.out.println("");
 		this.kor = kor;
-		//System.out.println("");
 	}
 
 	/**
@@ -62,9 +66,7 @@ public class Palya_Menedzser {
 	 * @param olajkeszlet
 	 */
 	public void setOlajkeszlet(int olajkeszlet) {
-		//System.out.println("");
 		this.olajkeszlet = olajkeszlet;
-		//System.out.println("");
 	}
 
 	/**
@@ -72,8 +74,6 @@ public class Palya_Menedzser {
 	 * @param ragacskeszlet
 	 */
 	public void setRagacskeszlet(int ragacskeszlet) {
-		//System.out.println("");
 		this.ragacskeszlet = ragacskeszlet;
-		//System.out.println("");
 	}
 }
