2585fc6fff405e7902f424229e0de6274f37d348
CsTomi <cstom@sch.bme.hu>
2015-03-30 20:44:23 +0200

    Update Ragacs.java

--- Ragacs.java
+++ Ragacs.java
@@ -2,20 +2,24 @@ package Class_Diagram;
 
 public class Ragacs extends Akadaly {
 
+	public Ragacs(){
+		this.elet = 4;
+	}
+	
 	public Ragacs(String nev){
 		Logger.logMetodusStart(nev, "(new Ragacs)");
 		this.obinev = nev;
 		Logger.tabok-=1;
 	}
+	
 	/**
 	 * A robot sebesség vektora a felére csökken.
 	 * @param r
 	 */
 	public void viselkedes(Robot r) {
 		Logger.logMetodusStart(obinev, "viselkedes("+r.obinev+")");
-		//System.out.println("");
+		this.eletCsokkent();
 		r.ragacsraLepett();
-		//System.out.println("");
 		Logger.logMetodusReturn("");
 	}
 }
